0fd824fd6f6395344523978250ce57cd28593db8
OrpheliePerrine <m.perrine@alustudent.com>
2026-01-20 21:24:21 +0000

    puppet

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # 0-strace_is_your_friend.pp
 # Fix WordPress 500 error by ensuring Apache can read files
 
-# Ensure /var/www/html exists with correct ownership and permissions
 file { '/var/www/html':
   ensure  => directory,
   owner   => 'www-data',
@@ -10,16 +9,6 @@ file { '/var/www/html':
   recurse => true,
 }
 
-# Ensure all files under /var/www/html are readable by Apache
-file { '/var/www/html':
-  recurse => true,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => File['/var/www/html'],
-}
-
-# Make sure Apache is running and enabled
 service { 'apache2':
   ensure  => running,
   enable  => true,
