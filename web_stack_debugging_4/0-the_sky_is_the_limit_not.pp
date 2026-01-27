2c2a1c60885f66b58d3b4741bd26fca1e654558d
OrpheliePerrine <m.perrine@alustudent.com>
2026-01-27 20:14:20 +0000

    Debugging

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,12 @@
-# This Puppet manifest increases Nginx worker connections to handle high traffic load
+# This Puppet manifest fixes Nginx configuration to handle high concurrent traffic
 
 exec { 'fix--for-nginx':
   command => 'sed -i "s/worker_connections [0-9]\\+/worker_connections 1024/" /etc/nginx/nginx.conf && nginx -s reload',
   path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
   onlyif  => 'grep -q "worker_connections" /etc/nginx/nginx.conf',
+}
+
+exec { 'print-zero':
+  command => '/bin/echo 0',
+  path    => ['/bin', '/usr/bin'],
 }
